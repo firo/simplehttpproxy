b981be317a9f813ae3b829f45b89c9dba6eead60
Firo <mail@firo.it>
2019-10-31 15:50:59 +0100

    Create proxy.js

---
+++ proxy.js
@@ -0,0 +1,18 @@
+var http = require('http');
+
+http.createServer(onRequest).listen(9000);
+
+function onRequest(client_req, client_res) {
+  console.log('serve: ' + client_req.url);
+
+  var proxy = http.request(client_req.url, function (res) {
+    client_res.writeHead(res.statusCode, res.headers)
+    res.pipe(client_res, {
+      end: true
+    });
+  });
+
+  client_req.pipe(proxy, {
+    end: true
+  });
+}
